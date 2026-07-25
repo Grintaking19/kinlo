a03ce9d77e836c237153a4a7324e1d3ec1346cf8
Grintaking19 <omarbadrh.ob@gmail.com>
2026-07-25 16:50:11 +0300

    :bug:Fixing minor variable bug in server.js

--- apps/server/src/server.js
+++ apps/server/src/server.js
@@ -2,13 +2,15 @@ import express from "express";
 import config from "./config.js";
 import cors from "cors";
 
+
+const app = express();
+const PORT = config.PORT || 3000;
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
 
-const app = express();
-const PORT = config.PORT || 3000;
 
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "Server is running!" });
