1db7d59bf436cc23a8a2d2cf39f6d2a53d382e22
Grintaking19 <omarbadrh.ob@gmail.com>
2026-08-06 16:24:17 +0300

    :triangular_ruler:Finished User, Post and Follow models

--- apps/server/prisma.config.ts
+++ apps/server/prisma.config.ts
@@ -2,7 +2,9 @@
 // npm install --save-dev prisma dotenv
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import dotenv from "dotenv";
 
+dotenv.config();
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
